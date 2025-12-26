1999ca80af6f4cf28ac80cc681cc25266ea999f0
itsagorski1 <jonahisaacgorski73@gmail.com>
2025-12-26 11:10:43 -0800

    define parseLines()

--- d3/d3.py
+++ d3/d3.py
@@ -6,4 +6,14 @@ def getJoltage(line):
     firstDigit = max(line[:(lineLen-1)])
     lastDigitIndex = line.index(max(line[(firstDigitIndex+1):]))
     lastDigit = max(line[(firstDigitIndex+1):])
-    return (firstDigitIndex, firstDigit, lastDigitIndex, lastDigit)
+    return str(firstDigit) + str(lastDigit)
+
+def parseLines():
+    with open(sys.argv[1], 'rt') as input_file:
+        input_lines = [x.strip() for x in input_file.readlines()]
+    for line in input_lines:
+        totalJoltage = int(getJoltage(line))
+    return totalJoltage
+
+if __name__ == '__main__':
+    print(parseLines())
